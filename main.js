59260536d76e6c6243b3671a6e54408dc2eb4a89
Cyr2 <dpessin@edenschool.fr>
2024-02-20 14:32:46 +0100

    add contact and about page

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const burgerBtn = document.querySelector('#burger');
-const menu = document.querySelector('.menu')
+const menu = document.querySelector('.menu');
+const cross = document.querySelectorAll('.cross');
+const recto = document.querySelectorAll('.recto');
+const verso = document.querySelectorAll('.verso');
 let burgerOpen = false;
 
 burgerBtn.addEventListener('click', () => {
@@ -15,3 +18,17 @@ burgerBtn.addEventListener('click', () => {
     burgerOpen = false;
   }
 });
+
+for (let i = 0; i < cross.length; i++) {
+  cross[i].addEventListener('click', () => {
+    if (recto[i].style.display === 'none') {
+      cross[i].classList.remove('active');
+      recto[i].style.display = 'flex';
+      verso[i].style.display = 'none';
+    } else {
+      cross[i].classList.add('active');
+      recto[i].style.display = 'none';
+      verso[i].style.display = 'flex';
+    }
+  });
+};
